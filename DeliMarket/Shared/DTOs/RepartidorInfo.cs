e0e4f831d9db142d52907015c6c5e1da21656376
Martin Ballasco Ramos <martinballasco@gmail.com>
2020-05-17 23:36:19 -0500

    Adding Registers (Repartidor,Mercado) & Models(Repartidor)

---
+++ DeliMarket/Shared/DTOs/RepartidorInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DeliMarket.Shared.DTOs
+{
+    public class RepartidorInfo
+    {
+
+        [Required]
+        [DisplayName]
+        public string Nombre { get; set; }
+        [Required]
+        public string DNI { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [Phone]
+        public string NumeroCel { get; set; }
+        [Required]
+        public string PlacaVehiculo { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
