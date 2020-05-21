91e8084ef8d87941253ef6278ff3128c3b8705ad
Martin Ballasco Ramos <martinballasco@gmail.com>
2020-05-21 10:04:19 -0500

    CambiosTProd

---
+++ DeliMarket/DeliMarket/Server/Migrations/20200520020913_CambiosTablaProducto.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DeliMarket.Server.Migrations
+{
+    public partial class CambiosTablaProducto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Trailer",
+                table: "Productos");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "89086180-b978-4f90-9dbd-a7040bc93f41",
+                column: "ConcurrencyStamp",
+                value: "13567fbd-dfa0-46a9-9c9a-dd4290524eab");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Trailer",
+                table: "Productos",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "89086180-b978-4f90-9dbd-a7040bc93f41",
+                column: "ConcurrencyStamp",
+                value: "617c428f-82d1-4ee7-9aee-bdae9a02764c");
+        }
+    }
+}
